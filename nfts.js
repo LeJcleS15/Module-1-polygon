e02736eb7a104026d1d62f40218d2d1ddfd8ad74
akankshateja <136678468+akankshateja@users.noreply.github.com>
2023-08-21 19:45:34 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'an apple',
+        image: 'QmYR9gecvLPqBKuDzcd6TBDRrZ1rGaxzURtixz84BT8auX'
+    },
+    {
+        name: '1',
+        description: 'an apple',
+        image: 'QmVtNvvE6vj4iFrrwaAoAEQ4J6Zk5RNE1XzMBQsgaJVFbk'
+    },//change
+    {
+        name: '2',
+        description: 'an apple',
+        image: 'QmVtNvvE6vj4iFrrwaAoAEQ4J6Zk5RNE1XzMBQsgaJVFbk'
+    },
+    {
+        name: '3',
+        description: 'an apple',
+        image: 'QmcieALX6FxXCMPQkSSFJRyVoJPhxsjdzJhaZSWxdurxzq'
+    },
+    {
+        name: '4',
+        description: 'an apple',
+        image: 'QmVseHZ1HtrZ5L9xE2uu1kGbqaJoqSBnAUxk4H6dk2vYEK'
+    },    
+];
+
+module.exports = nfts;
